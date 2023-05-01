3aee8ef012b92b77289e0c40ea69c306aa777acf
Alexander Grein <alexander.grein@gmail.com>
2023-05-01 16:38:30 +0200

    Update LinkModifier.php

--- Classes/TypoLink/LinkModifier.php
+++ Classes/TypoLink/LinkModifier.php
@@ -1,6 +1,26 @@
 <?php
 namespace FoT3\Jumpurl\TypoLink;
 
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\StringUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Frontend\Http\UrlProcessorInterface;
+use TYPO3\CMS\Frontend\Typolink\PageLinkBuilder;
+
 class LinkModifier
 {
 }
